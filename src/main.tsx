c4830253f9953613456a10286e1f30dc189ea695
João Silva <z4kreyyt@gmail.com>
2023-10-26 19:13:43 +0100

    Adding Global Styles
    B

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { RoutesList } from "./router/routes";
+import './global/global.css';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
