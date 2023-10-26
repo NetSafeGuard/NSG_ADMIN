c6076a4165cee58338b6e60188aaa9db08b5252f
João Silva <z4kreyyt@gmail.com>
2023-10-26 19:58:56 +0100

    Adding Poppins Font + Some styles

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,14 @@
 import * as C from './style';
+import Logo from '../../assets/Logo.png'
 
 export const LoginPage = () => {
     return (
         <C.LoginContainer>
             <C.Content>
-                Hello Page
+                <C.LogoContainer>
+                    <C.Logo src={Logo} alt="Logo" />
+                    <C.LogoText>NetSafeGuard</C.LogoText>
+                </C.LogoContainer>  
             </C.Content>
         </C.LoginContainer>
     )
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -5,8 +5,29 @@ export const LoginContainer = styled.div`
     width: 100%;
     height: 100vh;
     background-color: #202227;
+    flex-direction: column;
 `
 
 export const Content = styled.div`
     margin: 22px;
+`
+
+export const LogoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+`
+
+export const Logo = styled.img`
+    width: 40px;
+    height: 40px;
+`
+
+export const LogoText = styled.span`
+    color: #FFF;
+    font-family: 'Poppins', sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
 `
