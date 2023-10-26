4185a1514cdf11709d8f53ca6ab868c5fd6db71d
João Silva <z4kreyyt@gmail.com>
2023-10-26 23:19:37 +0100

    Error Component

---
+++ src/components/error/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ErrorContainer = styled.div`
+    overflow: hidden;
+    position: relative;
+`
+
+export const ErrorContent = styled.div`
+    font-family: 'Poppins', sans-serif;
+    color: #CE524D;
+    font-size: 12px;
+`
