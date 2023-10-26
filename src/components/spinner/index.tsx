8cf70e8671264cbac6afaf1009a4679c06751048
João Silva <z4kreyyt@gmail.com>
2023-10-26 22:40:51 +0100

    Spinner Component

---
+++ src/components/spinner/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './style';
+
+
+export const Spinner = () => {
+    return (
+        <C.SpinnerContainer>
+            <C.Spinner />
+        </C.SpinnerContainer>
+    )
+};
---
+++ src/components/spinner/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const SpinnerContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Spinner = styled.div`
+    width: 20px;
+    height: 20px;
+    border: 1px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 1px solid #3498db;
+    animation: spin 1s linear infinite;
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+`
