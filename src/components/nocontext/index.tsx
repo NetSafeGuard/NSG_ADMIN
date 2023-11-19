50c0793bc5d887baad9e8a2a0554d888a6d7c06f
João Silva <75593113+imjoaosilva@users.noreply.github.com>
2023-11-19 12:44:19 +0000

    Adding server api

--- src/components/nocontext/index.tsx
+++ src/components/nocontext/index.tsx
@@ -3,14 +3,13 @@ import { useEffect } from 'react';
 export const NoContextMenu = () => {
   const disableContextMenu = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
-  };
+  };  
 
   useEffect(() => {
-    document.addEventListener('contextmenu', disableContextMenu);
-
-    return () => {
-      document.removeEventListener('contextmenu', disableContextMenu);
-    };
+      document.addEventListener('contextmenu', disableContextMenu); 
+      return () => {
+        document.removeEventListener('contextmenu', disableContextMenu);
+      };
   }, []);
 
   return null;
