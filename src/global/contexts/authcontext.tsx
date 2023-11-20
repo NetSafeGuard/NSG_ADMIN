682897a8ac22e16230fa75183be6c88b7f61d22d
Z4dEV <z4kreyyt@gmail.com>
2023-11-20 17:38:40 +0000

    Adding timer on notifcarion by Bruno

--- src/global/contexts/authcontext.tsx
+++ src/global/contexts/authcontext.tsx
@@ -52,6 +52,10 @@ export function RemoveNotification() {
         container: "top-right",
         animationIn: ["animate__animated", "animate__fadeIn"],
         animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+            duration: 5000,
+            onScreen: true
+        }
     });
 }
 
