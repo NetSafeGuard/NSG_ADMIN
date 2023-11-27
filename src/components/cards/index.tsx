c9a0542f7044a69faf4750daf4b36156c53091d1
Z4dEV <z4kreyyt@gmail.com>
2023-11-27 16:19:02 +0000

    Changing Card Title TEXT

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -9,7 +9,7 @@ export const Cards = () => {
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">
-            Exames Realizados
+            Exames Registrados
           </CardTitle>
           <MdEventAvailable className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
