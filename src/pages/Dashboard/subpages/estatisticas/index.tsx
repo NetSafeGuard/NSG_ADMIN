417399930641dc1e537b1af58a25b528135e0fa5
Z4dEV <z4kreyyt@gmail.com>
2023-11-29 10:44:59 +0000

    Remove unused import

--- src/pages/Dashboard/subpages/estatisticas/index.tsx
+++ src/pages/Dashboard/subpages/estatisticas/index.tsx
@@ -1,6 +1,5 @@
 import * as C from "./style";
 import { Cards } from "@/components/cards";
-import { InfoHook } from "@/services/hooks/InfoHook";
 
 export const EstatisticasPage = () => {
   return (
