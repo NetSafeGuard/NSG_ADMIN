7031a7e967f0151f58fa0071655eaca089cdae02
Z4 <z4kreyyt@gmail.com>
2023-12-17 17:33:07 +0000

    Adding components

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,24 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { RoutesList } from "./router/routes";
-import './global/global.css';
+import "./global/global.css";
 import { AuthProvider } from "./global/contexts/AuthContext";
 import { ReactNotifications } from "react-notifications-component";
 import { NoContextMenu } from "./components/nocontext";
-import 'react-notifications-component/dist/theme.css'
-import 'react-tooltip/dist/react-tooltip.css';
-import 'react-loading-skeleton/dist/skeleton.css'
+import "react-notifications-component/dist/theme.css";
+import "react-tooltip/dist/react-tooltip.css";
+import "react-loading-skeleton/dist/skeleton.css";
 import { TitleBar } from "./components/titlebar";
+import { Toaster } from "@/components/ui/toaster";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
-    <TitleBar/>
+    <TitleBar />
     <BrowserRouter>
       <AuthProvider>
         <NoContextMenu />
         <ReactNotifications />
+        <Toaster />
         <RoutesList />
       </AuthProvider>
     </BrowserRouter>
