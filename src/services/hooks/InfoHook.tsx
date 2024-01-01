15c9e57bc7ad165cb43353dfb1d3234c7d2a451a
Z4 <z4kreyyt@gmail.com>
2024-01-01 03:15:12 +0000

    Adding revalidadeonFocus & reduce refreshInterval

--- src/services/hooks/InfoHook.tsx
+++ src/services/hooks/InfoHook.tsx
@@ -1,13 +1,16 @@
-import useSWR  from 'swr';
-import { api } from '../api';
+import useSWR from "swr";
+import { api } from "../api";
 
-const fetcher = (url: string) => api.get(url).then(res => res.data.data)
+const fetcher = (url: string) => api.get(url).then((res) => res.data.data);
 
 export const InfoHook = () => {
-    const { data, error } = useSWR('/info/', fetcher, {refreshInterval: 30000})
-    return {
-        users: data?.users,
-        isLoading: !error && !data,
-        error: error
-    }
-}
+  const { data, error } = useSWR("/info/", fetcher, {
+    refreshInterval: 4000,
+    revalidateOnFocus: true,
+  });
+  return {
+    users: data?.users,
+    isLoading: !error && !data,
+    error: error,
+  };
+};
