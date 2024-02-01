0a86d638c419412a19e246adef810fc706503e6a
Z4 <z4kreyyt@gmail.com>
2024-02-01 00:53:41 +0000

    Adding NoContext again

--- src/components/nocontext/index.tsx
+++ src/components/nocontext/index.tsx
@@ -1,5 +1,17 @@
 import { useEffect } from "react";
 
 export const NoContextMenu = () => {
+  const disableContextMenu = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+  };
 
+  useEffect(() => {
+    document.addEventListener("contextmenu", disableContextMenu);
+
+    return () => {
+      document.removeEventListener("contextmenu", disableContextMenu);
+    };
+  }, []);
+
+  return null;
 };
