0c137bcc23ae4323bee61b452e34d0a6f9ba4e2e
Z4 Dev <z4kreyyt@gmail.com>
2024-02-16 20:19:19 +0000

    Update API and socket URLs

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -3,7 +3,13 @@ import { io } from "socket.io-client";
 const createSocketInstance = async () => {
   const token = localStorage.getItem("nsg_token");
 
-  const socket = io("http://localhost:3001", {
+  // const socket = io("http://localhost:3001", {
+  //   query: {
+  //     token: token,
+  //   },
+  // });
+
+  const socket = io("https://wss.netsafeguard.cloud", {
     query: {
       token: token,
     },
