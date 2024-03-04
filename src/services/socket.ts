7cf88f91989256a8478f9e7ae456f853b67c397a
=Z4 <z4kreyyt@gmail.com>
2024-03-04 22:31:15 +0000

    Commented out local socket connection and added production socket connection

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -3,11 +3,11 @@ import { io } from "socket.io-client";
 const createSocketInstance = async () => {
   const token = localStorage.getItem("nsg_token");
 
-  // const socket = io("http://localhost:3001", {
-  //   query: {
-  //     token: token,
-  //   },
-  // });
+/*   const socket = io("http://localhost:3001", {
+    query: {
+      token: token,
+    },
+  }); */
 
   const socket = io("https://wss.netsafeguard.cloud", {
     query: {
