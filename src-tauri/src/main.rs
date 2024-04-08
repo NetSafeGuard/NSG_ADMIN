742c7e6b418061f354272c8f51ad57e672a12728
Z4dEV <z4kreyyt@gmail.com>
2024-04-08 10:59:13 +0100

    Update dependencies and remove unused code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use window_shadows::set_shadow;
-use tauri::Manager;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
