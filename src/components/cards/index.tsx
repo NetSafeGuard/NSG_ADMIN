b2b855599c1805d55061ee5047472f67cab7067d
Z4dEV <z4kreyyt@gmail.com>
2024-04-08 12:02:30 +0100

    Update card title to reflect activities instead of exams

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -31,7 +31,7 @@ export const Cards = () => {
       <StyledCard>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">
-            Exames Registrados
+            Atividades Registradas
           </CardTitle>
           <MdEventAvailable className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
