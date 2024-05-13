1b647afe410cfb03068b7ce7659566e029f0a9b9
Z4dEV <z4kreyyt@gmail.com>
2024-05-13 16:01:33 +0100

    chore: Add Separator component and update npm dependencies

--- src/pages/Dashboard/subpages/activity/index.tsx
+++ src/pages/Dashboard/subpages/activity/index.tsx
@@ -16,6 +16,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+import MultiSelectFormField from "@/components/ui/multi-select";
+import { useForm, Controller } from "react-hook-form";
 import {
   Sheet,
   SheetClose,
@@ -26,11 +28,44 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { DateTimePicker } from "@/components/ui/datetimer-picker";
+import { Group } from "@/@types/Group";
+import { useContext } from "react";
+import { GroupsContext } from "@/contextapi/groups.context";
+import { useState } from "react";
 
 export const ActivityPage = () => {
-  const [date,setDate] = useState();
+
+  const [open,setOpen] = useState(false);
+  const [createActivity, setCreateActivity] = useState(false);
+  const {groups} = useContext(GroupsContext);
+
+  type CreateData = {
+    title: string;
+    description: string;
+    date: Date;
+    groups: Group[];
+  };
+
+  const DataSchema = yup.object().shape({
+    title: yup.string().required("O título é obrigatório"),
+    description: yup.string().required("A descrição é obrigatória"),
+    date: yup.date().required("A data é obrigatória"),
+    groups: yup.array().required("O grupo é obrigatório"),
+  });
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<CreateData>({
+    resolver: yupResolver(DataSchema),
+  });
 
   const activitys = [
     {
@@ -76,7 +111,7 @@ export const ActivityPage = () => {
         ))}
       </C.ActivityContainer>
       <C.ButtonContainer>
-        <DropdownMenu>
+        <DropdownMenu open={open} onOpenChange={setOpen}>
           <DropdownMenuTrigger asChild>
             <Button variant="outline">
               <PlusIcon />
@@ -86,62 +121,11 @@ export const ActivityPage = () => {
             <DropdownMenuLabel>Criação de Atividade</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <Sheet>
-                <SheetTrigger asChild>
-                  <p className="flex items-center gap-2 px-2 py-2 text-sm rounded-md cursor-pointer hover:bg-accent hover:bg-opacity-10">
-                    <span>URL</span>
-                  </p>
-                </SheetTrigger>
-                <SheetContent className="w-[500px] sm:[100%] mt-8">
-                  <C.ContainerScroll>
-                    <SheetHeader>
-                      <SheetTitle>Criar Atividade</SheetTitle>
-                      <SheetDescription>
-                        Preencha os campos abaixo para criar uma atividade
-                      </SheetDescription>
-                    </SheetHeader>
-                    <div className="grid gap-4 py-4">
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-left">
-                          Titulo
-                        </Label>
-                        <Input
-                          id="name"
-                          value="Pedro Duarte"
-                          className="col-span-3"
-                        />
-                      </div>
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-left">
-                          Descrição
-                        </Label>
-                        <Input
-                          id="username"
-                          value="@peduarte"
-                          className="col-span-3"
-                        />
-                      </div>
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-left">
-                          Data de ínicio
-                        </Label>
-                        <DateTimePicker/>
-                      </div>
-                      <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-left">
-                          Data de término
-                        </Label>
-                        <DateTimePicker/>
-                      </div>
-                    </div>
-                    <SheetFooter>
-                      <SheetClose asChild>
-                        <Button type="submit">Save changes</Button>
-                      </SheetClose>
-                    </SheetFooter>
-                  </C.ContainerScroll>
-                </SheetContent>
-              </Sheet>
+              <DropdownMenuItem onClick={() => setCreateActivity(true)} className="cursor-pointer">
+                <p className="flex items-center text-sm rounded-md hover:bg-accent hover:bg-opacity-10">
+                  <span>URL</span>
+                </p>
+              </DropdownMenuItem>
               <DropdownMenuItem>
                 Arquivo (WIP)
                 <DropdownMenuShortcut>Desativado</DropdownMenuShortcut>
@@ -150,6 +134,81 @@ export const ActivityPage = () => {
           </DropdownMenuContent>
         </DropdownMenu>
       </C.ButtonContainer>
+      <Sheet open={createActivity} onOpenChange={setCreateActivity}>
+        <SheetContent className="w-[500px] sm:[100%] mt-8">
+          <C.ContainerScroll>
+            <SheetHeader>
+              <SheetTitle>Criar Atividade</SheetTitle>
+              <SheetDescription>
+                Preencha os campos abaixo para criar uma atividade
+              </SheetDescription>
+            </SheetHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-left">
+                  Titulo
+                </Label>
+                <Input
+                  id="name"
+                  value="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-left">
+                  Descrição
+                </Label>
+                <Input
+                  id="username"
+                  value="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-left">
+                  Data de ínicio
+                </Label>
+                <DateTimePicker/>
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-left">
+                  Data de término
+                </Label>
+                <DateTimePicker/>
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4 w-full">
+                <Label htmlFor="role" className="text-right flex gap-1">
+                  Grupos
+                </Label>
+                <Controller
+                  name="groups"
+                  control={control}
+                  render={({ field }) => (
+                    <MultiSelectFormField
+                    className=""
+                    options={groups.map((group) => ({
+                      label: group.name,
+                      value: group.name,
+                    }))}
+                    onValueChange={field.onChange}
+                    placeholder="Selecione os grupos"
+                    variant="inverted"
+                    style={{ width: "230px" }}
+                  />
+                  )}
+                />
+              </div>
+            </div>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button 
+                style={{ background: "#1b4c70" }}
+                type="submit">Criar atividade</Button>
+              </SheetClose>
+            </SheetFooter>
+          </C.ContainerScroll>
+        </SheetContent>
+      </Sheet>
     </C.Container>
   );
 };
