9699a0c6345551d718ba7d1226b137dd6c3bbc19
Z4dEV <z4kreyyt@gmail.com>
2024-06-03 15:47:29 +0100

    chore: Remove commented code and update Activity interface in Activity.ts

--- src/@types/Activity.ts
+++ src/@types/Activity.ts
@@ -1,12 +1,3 @@
-// const DataSchema = yup.object().shape({
-//     title: yup.string().required(),
-//     description: yup.string().required(),
-//     startdate: yup.date().required(),
-//     enddate: yup.date().required(),
-//     groups: yup.array().of(yup.string()),
-//   });
-
-
 export interface Activity {
     title: string;
     description: string;
